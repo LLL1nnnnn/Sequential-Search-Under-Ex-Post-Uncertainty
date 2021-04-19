d33d53c36fb74347a33eceb7f2fcc8c07463f078
Yilin Li <YilinLi@yilins-mbp.lan>
2021-04-18 22:42:48 -0700

    comment

--- demographics/__init__.py
+++ demographics/__init__.py
@@ -21,19 +21,19 @@ class Player(BasePlayer):
     ucscID = models.IntegerField(label='What is your UCSC student ID number', min=0000000, max=9999999)
     email = models.StringField(label='What is your UCSC email address?')
     # age = models.IntegerField(label='What is your age?', min=13, max=100)
-    gender = models.StringField(
-        choices=[['Male', 'Male'], ['Female', 'Female'], ['Other', 'Other']], 
-        label='What is your gender?',
-        # widget=widgets.RadioSelect,
-    )
-    ethnicity = models.StringField(
-        choices=[['American Indian or Alaska Native', 'American Indian or Alaska Native'], 
-        ['Asian', 'Asian'], ['Black or African American', 'Black or African American'], 
-        ['Native Hawaiian or Other Pacific Islander', 'Native Hawaiian or Other Pacific Islander'], 
-        ['White', 'White'], ['Other', 'Other'] , ['Prefer not to say', 'Prefer not to say']], 
-        label='What is your ethnic group?',
-        # widget=widgets.RadioSelect,
-    )
+    # gender = models.StringField(
+    #     choices=[['Male', 'Male'], ['Female', 'Female'], ['Other', 'Other']], 
+    #     label='What is your gender?',
+    #     # widget=widgets.RadioSelect,
+    # )
+    # ethnicity = models.StringField(
+    #     choices=[['American Indian or Alaska Native', 'American Indian or Alaska Native'], 
+    #     ['Asian', 'Asian'], ['Black or African American', 'Black or African American'], 
+    #     ['Native Hawaiian or Other Pacific Islander', 'Native Hawaiian or Other Pacific Islander'], 
+    #     ['White', 'White'], ['Other', 'Other'] , ['Prefer not to say', 'Prefer not to say']], 
+    #     label='What is your ethnic group?',
+    #     # widget=widgets.RadioSelect,
+    # )
     participantID = models.StringField(label='What is your participant ID (Zoom Username)?')
     venmoID = models.StringField(label='What is your Venmo ID?')
     searchcomment = models.LongStringField(label='Given a searched list, which item will you purchase in the search task?')
@@ -45,7 +45,7 @@ class Player(BasePlayer):
 # PAGES
 class Demographics(Page):
     form_model = 'player'
-    form_fields = ['last_name', 'first_name', 'ucscID', 'email', 'gender', 'ethnicity', 'participantID', 'venmoID', 'searchcomment', 'stopcomment', 'suggest']
+    form_fields = ['last_name', 'first_name', 'ucscID', 'email', 'participantID', 'venmoID', 'searchcomment', 'stopcomment', 'suggest']
 
 
 class Results(Page):
